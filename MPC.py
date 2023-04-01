576e876bb16aeab3f6e2d3d17b63dfeb2b947933
darrrt <shaojunxu123@126.com>
2023-04-01 21:34:05 +0800

    add another spline sample

--- MPC.py
+++ MPC.py
@@ -119,19 +119,10 @@ class MPCPredict():
 
         self.xpos=np.interp.int
 
-        Size = 30
-        x = np.arange(Size)
-        y = np.random.randint(1, Size, Size)
-
-        #平滑前
-        plt.plot(x, y,'r')
-        plt.show()
-
         #平滑处理后
-        x_smooth = np.linspace(x.min(), x.max(), 500)  # np.linspace 等差数列,从x.min()到x.max()生成300个数，便于后续插值
-        y_smooth = make_interp_spline(x, y)(x_smooth)
-        plt.plot(x_smooth, y_smooth,'b')
-        plt.show()
+        x_smooth = np.linspace(0, self.steps, self.steps)  # np.linspace 等差数列,从x.min()到x.max()生成300个数，便于后续插值
+        y_smooth = make_interp_spline(t, path_x)(x_smooth)
+
 
         pass 
     def Control(self):
--- main_plan_once.py
+++ main_plan_once.py
@@ -16,6 +16,27 @@ from scipy.signal import savgol_filter
 from scipy.interpolate import make_interp_spline
 from math import factorial
 
+# another bassel
+
+# import numpy as np
+# from matplotlib import pyplot as plt
+# from scipy.interpolate import make_interp_spline
+
+# Size = 30
+# x = np.arange(Size)
+# y = np.random.randint(1, Size, Size)
+
+# #平滑前
+# plt.plot(x, y,'r')
+# plt.show()
+
+# #平滑处理后
+# x_smooth = np.linspace(x.min(), x.max(), 500)  # np.linspace 等差数列,从x.min()到x.max()生成300个数，便于后续插值
+# y_smooth = make_interp_spline(x, y)(x_smooth)
+# plt.plot(x_smooth, y_smooth,'b')
+# plt.show()
+
+
 def evaluate_bezier(points, total):
 	def bezier_Func(points):
 		def C_(n, k):
